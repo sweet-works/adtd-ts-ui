079956280776b7cdd8cc91b13a86618dbc055f5f
WYJTang <643392718@qq.com>
2020-09-21 14:29:40 +0800

    基本样式配置

--- vking-ui/src/App.tsx
+++ vking-ui/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './styles/index.scss';
 
 function App() {
+  let a = '123';
+  if (a === '123') {
+    console.log(123);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>hello world</h1>
+        <h2>hello world</h2>
+        <h3>hello world</h3>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
