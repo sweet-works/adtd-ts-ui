8de23a4a932f00294d105e902b0dcc5b3e68e63c
WYJTang <643392718@qq.com>
2020-09-21 16:46:00 +0800

    按钮样式模块化

--- vking-ui/src/App.tsx
+++ vking-ui/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './styles/index.scss';
-
+import { Button } from './components/index';
+import { ButtonType, ButtonSize } from './components/Button/button';
 function App() {
   let a = '123';
   if (a === '123') {
@@ -14,7 +15,13 @@ function App() {
         <h1>hello world</h1>
         <h2>hello world</h2>
         <h3>hello world</h3>
-        <p>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Larg}>Larg hello world</Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Samll}>Samll hello world</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Samll}>Danger hello world</Button>
+        <Button btnType={ButtonType.Primary} disabled={true}>hello world</Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com'> link hello world</Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled={true}> link hello world</Button>
+        <p> 
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
---
+++ vking-ui/src/components/index.tsx
@@ -0,0 +1,4 @@
+import Button from './Button/button';
+export {
+    Button
+}
